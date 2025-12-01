af6b4ed28ac96b84ee79ffe9aea83608b7697982
DanHolli <18298283+DanHolli@users.noreply.github.com>
2025-12-01 15:57:58 -0700

    Fixing a broken reference in the Android stack

--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -5,7 +5,7 @@ package ffi
 import android.content.Context
 import java.nio.ByteBuffer
 
-import com.carriez.flutter_hbb.RdClipboardManager
+import us.danholli.rd4f.RdClipboardManager
 
 object FFI {
     init {
